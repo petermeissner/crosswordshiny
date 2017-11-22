f03e42e869565c25eb6586feac286208c423b7c1
Peter Meissner <retep.meissner@gmail.com>
2017-11-22 21:33:28 +0100

    init

---
+++ R/cw_as_standalone.R
@@ -0,0 +1,16 @@
+#' Turn crosswords into standalone
+#'
+#' @param crossword a crossword
+#'
+#' @return
+#'
+#' Either write to a file or returns the result as character vector to be
+#' handled by the user.
+#'
+#' @export
+#'
+#'
+#'
+cw_as_standalone <- function(crossword){
+
+}
